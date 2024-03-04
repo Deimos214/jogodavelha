6c73bbb1ba542ac2b831fcb2a1545c3370ac1c11
Usuario <Usuario@CPD03-M09>
2024-03-04 15:55:20 -0300

    Adicionar arquivos de projeto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button disparador = (Button)sender;
+
+            disparador.IsEnabled = false;
+
+            if (vez == "0")
+            {
+                disparador.Text = "0";
+                vez = "X";
+
+            } else
+            {
+                disparador.Text="X";
+            }
+
+    }
+}
+}
